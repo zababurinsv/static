bf891027d2cf4def0ec2ab0a080062b0514e9ba2
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-03-07 02:39:27 +0300

    update

---
+++ html/components/component_modules/performance/module-performance.mjs
@@ -0,0 +1,1 @@
+import now from"/static/html/components/component_modules/performance/module-now.mjs";import measure from"/static/html/components/component_modules/performance/module-measure.mjs";import mark from"/static/html/components/component_modules/performance/module-mark.mjs";export default{now:now,mark:mark,measure:measure};
