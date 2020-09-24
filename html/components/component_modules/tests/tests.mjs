2110a0bb3e0605316f33bc6e54a56c058268f9c8
Zababurin <s.zababurin.v@gmail.com>
2020-09-25 00:48:49 +0300

    update

--- html/components/component_modules/tests/tests.mjs
+++ html/components/component_modules/tests/tests.mjs
@@ -9,6 +9,7 @@ export default ( obj ={ _:'default' } ) =>{
         }else{
             sw =  location.pathname
         }
+        console.log('###################', sw)
         switch (sw) {
             case 'localhost:1111':
                 await tests('/tests/game/monopoly.mjs','tests')
