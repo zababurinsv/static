437beaf14aa8d4231feb658052901b42391be6d0
Zababurin <s.zababurin.v@gmail.com>
2020-09-27 17:02:53 +0300

    update

--- html/components/image-index/external/index.mjs
+++ html/components/image-index/external/index.mjs
@@ -1,6 +1,8 @@
 import psd from '/static/html/components/component_modules/bundle/psd/docs.mjs'
 
 export default async (v,p,c,obj,r) =>{
+    // let include = obj.this.shadowRoot.querySelector('.design')
+    // include['attachShadow']({mode: 'open'})
     let obj01 = await psd.init(true,['psd'],'red',obj,'psd')
     let obj02 = await psd.write(true,['psd'],'red',obj,'psd-write')
     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',obj01.r)
