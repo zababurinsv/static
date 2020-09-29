a300dc238b1b6cd57574fdbea0917e1f29dc4ccf
Zababurin <s.zababurin.v@gmail.com>
2020-09-29 04:19:17 +0300

    update

--- html/components/image-index/external/index.mjs
+++ html/components/image-index/external/index.mjs
@@ -6,7 +6,22 @@ export default async (v,p,c,obj,r) =>{
     let include = obj.this.shadowRoot.querySelector('.design-property-views')
     include['attachShadow']({mode: 'open'})
     let main = document.createElement('div')
-
+    let header = obj['this']['shadowRoot'].querySelector('.design-property-views-header')
+    let body = obj['this']['shadowRoot'].querySelector('.design-property-views-body')
+    let footer = obj['this']['shadowRoot'].querySelector('.design-property-views-footer')
+    header['attachShadow']({mode: 'open'})
+    body['attachShadow']({mode: 'open'})
+    footer['attachShadow']({mode: 'open'})
+    let shadowHeader = header.querySelector('.root')
+    let shadowBody = body.querySelector('.root')
+    let shadowFooter = footer.querySelector('.root')
+    header.shadowRoot.appendChild(shadowHeader)
+    body.shadowRoot.appendChild(shadowBody)
+    footer.shadowRoot.appendChild(shadowFooter)
+    obj.views = {}
+    obj.views.header = header
+    obj.views.body = body
+    obj.views.footer = footer
     for(let item of config.slot){
         let div = document.createElement('div')
         let slot = document.createElement('slot')
