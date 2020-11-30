4d58c0f14ba230477d65adf567f6bf6c1e0b604d
Zababurin <s.zababurin.v@gmail.com>
2020-11-30 22:03:29 +0300

    update

--- html/components/lib-markdown/external/index.mjs
+++ html/components/lib-markdown/external/index.mjs
@@ -1,8 +1,12 @@
 import events from '/static/html/components/lib-markdown/external/events.mjs'
+import IDBFS from '/static/html/components/lib-markdown/external/wasm/idbfs.mjs'
 import isEmpty from '/static/html/components/component_modules/isEmpty/isEmpty.mjs'
-import Markdown from '/static/html/components/lib-markdown/external/wasm/sundown.mjs'
 import Parser from '/static/html/components/component_modules/bundle/html2json/html2json.index.mjs'
+import * as md2html from  '/static/html/components/lib-markdown/external/wasm/markdown.es.mjs'
 export default async (v,p,c,obj,r) => {
+    var _scriptDir = import.meta.url;
+    console.log('~~~~_scriptDir~~~~~~~', _scriptDir)
+    await md2html.ready
     let output = [];
     let html = obj['this']['shadowRoot'].querySelector('.markdown__html')
     let htmlstr = obj['this']['shadowRoot'].querySelector('.markdown__string')
@@ -22,7 +26,7 @@ export default async (v,p,c,obj,r) => {
         console.log('file save')
     });
     window.onbeforeunload = () => { fsSave(); }
-    let object=await Markdown({
+    let idbfs=await IDBFS({
         preInit() {},
         onRuntimeInitialized() {
             try {
@@ -46,14 +50,13 @@ export default async (v,p,c,obj,r) => {
         self.value = md
         updateUI()
     }
-
     function download() {
         let name = prompt('Введите название файла', 'default');
-        let dir = object.FS.readdir("/data")  
+        let dir = idbfs.FS.readdir("/data")  
         let filename = `${name}.md`
         let text = ''
         if(dir.find(item => item === 'data.md')) {
-            text =  object.FS.readFile("/data/data.md",{ encoding: "utf8" });
+            text =  idbfs.FS.readFile("/data/data.md",{ encoding: "utf8" });
         }
         var element = document.createElement('a');
         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -63,7 +66,6 @@ export default async (v,p,c,obj,r) => {
         element.click();
         document.body.removeChild(element);
     }
-
     function upload(event) {
         console.log(event.path[0].files[0])
         let reader = new FileReader();
@@ -79,16 +81,15 @@ export default async (v,p,c,obj,r) => {
             console.log(reader.error);
           };
     }
-
     function markdownToHTML(markdown) {
         output = [];
-        object.FS.writeFile('/data/data.md',markdown)
-        object.callMain(["/data/data.md"]);
+        idbfs.FS.writeFile('/data/data.md',markdown)
+        output = md2html.parse(markdown)
         return output;
     }
     function saveMd () {
         let output = markdownToHTML(self.value);
-        html.innerHTML = output.join(" ");
+        html.innerHTML = output;
         htmlstr.innerText = Parser.json(Parser.parse(html.innerHTML))
         fsSave()
     }
@@ -102,9 +103,9 @@ export default async (v,p,c,obj,r) => {
         }
     }
     await fsLoad()
-    let dir = object.FS.readdir("/data")  
+    let dir = idbfs.FS.readdir("/data")  
     if(dir.find(item => item === 'data.md')) {
-        let mdfs =  object.FS.readFile("/data/data.md",{ encoding: "utf8" });
+        let mdfs =  idbfs.FS.readFile("/data/data.md",{ encoding: "utf8" });
         if(!isEmpty(mdfs)) { self.value = mdfs }
     }
     updateUI()
