18fe964ee75404a1946fe53091ff460f72025884
Zababurin <s.zababurin.v@gmail.com>
2020-12-05 00:05:52 +0300

    update

--- html/components/manager-board/external/events.mjs
+++ html/components/manager-board/external/events.mjs
@@ -1,5 +1,25 @@
-//import draggable from '/static/html/components/component_modules/bundle/draggable/draggable.index.mjs'
 export default async (v,p,c,obj,r) => {
-
+    let worker = new Worker("/distrib/fs/worker_blobs.mjs");
+    let blob = new Blob(['blob data']);
+    worker.onmessage = msg => {
+        console.log("[Main thread] Got message back:", msg.data);
+    }
+    worker.postMessage(blob);
+    // let wmfs= await WMFS({
+        // preInit() {},
+        // onRuntimeInitialized() {
+            // try {
+                // isEmpty(window.FS)? window.FS = this.FS:window.FS 
+                // const fsSetup = path => {
+                    // this.FS.mkdir(path);
+                    // this.FS.mount(this.FS.filesystems.IDBFS, {}, path);
+                // };
+                // fsSetup("/data");
+            // } catch (e) {
+                // console.error('error', e)
+            // }
+        // },
+        // print: d => output.push(d),
+    // })
 
 }
