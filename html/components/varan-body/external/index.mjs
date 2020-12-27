1d250c35d5f5e00b100a593496b0cc41231969f0
Zababurin <s.zababurin.v@gmail.com>
2020-12-27 23:56:03 +0300

    update

--- html/components/varan-body/external/index.mjs
+++ html/components/varan-body/external/index.mjs
@@ -1,4 +1,4 @@
-import events from '/static/html/components/varan-header/external/events.mjs'
+import events from '/static/html/components/varan-body/external/events.mjs'
 export default async (v,p,c,obj,r) => {
     events(v,p,c,obj,r)
 }
