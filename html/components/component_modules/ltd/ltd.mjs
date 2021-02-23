f6338e12e479a913a705422062c3627be492fd0d
Zababurin <s.zababurin.v@gmail.com>
2021-02-23 07:03:31 +0300

    update

--- html/components/component_modules/ltd/ltd.mjs
+++ html/components/component_modules/ltd/ltd.mjs
@@ -1,10 +1,14 @@
 import core from '/static/html/components/component_modules/ride/index.mjs'
-
+/**
+ * Язык тернарного описания
+ * @namespace ЯТО
+ */
 export default async () =>{
   return {
     /**
      * Function return Bytes
      * @param {string} s
+     * @return Bytes
      */
     toBytes: (s) => {
 
