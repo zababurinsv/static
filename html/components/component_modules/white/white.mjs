8ddfc4cd2002d9a90c03bde38ebfd15136537e1e
Zababurin <s.zababurin.v@gmail.com>
2021-03-20 03:56:31 +0300

    update

--- html/components/component_modules/white/white.mjs
+++ html/components/component_modules/white/white.mjs
@@ -1,3 +1,3 @@
-export default [`http://localhost:6040`, `http://localhost:7030`, 'https://zababurinsv.github.io/blockchain-dex','https://zababurinsv.github.io/authorization-waves'
+export default [`http://localhost:6040`, `http://localhost:7030`, 'https://zababurinsv.github.io/dex','https://zababurinsv.github.io/authorization-waves'
 
 ]
