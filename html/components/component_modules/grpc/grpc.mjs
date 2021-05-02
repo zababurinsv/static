8132eeadb37260bd8597be94bd572e0ba1d2140c
Zababurin <s.zababurin.v@gmail.com>
2021-05-02 06:17:32 +0300

    update

--- html/components/component_modules/grpc/grpc.mjs
+++ html/components/component_modules/grpc/grpc.mjs
@@ -23,14 +23,3 @@ export default (proto = {},hostname = 'http://localhost:8080') => {
         resolve(sys)
    });
 }
-
-
-// var echoService = new EchoServiceClient('http://localhost:8080');
-//
-// var request = new EchoRequest();
-// request.setMessage('Hello World!');
-//
-// echoService.echo(request, {}, function(err, response) {
-//     ...
-// });
-//
