84b4c5a74d4516c62e0cd6bf74088723d4926c28
Zababurin <s.zababurin.v@gmail.com>
2021-05-10 00:46:52 +0300

    update

--- html/components/component_modules/proxy/proxy.mjs
+++ html/components/component_modules/proxy/proxy.mjs
@@ -1,1 +1,67 @@
-import store from"../../../../../../static.frontend/static/html/components/component_modules/staticProperty/staticProperty.mjs";let handler={get:function(t,e){return console.log("~~~!!!~~~",t,e),t[e]},set:function(t,e,o){return t[e]=o,console.log("~~~!0!~~~",t[e],o),!0}};export default(t,e,...o)=>new Promise(function(n,r){let c=t=>{console.log("~~~ err  ~~~",t),r(t)};switch(e){case"init":(async(t,r,s)=>{try{switch(console.log(`app(${e}[(${t.input})${t[r]}]property)`),t[r]){case"queue":(async(t,e,o)=>{try{let t=await store({input:"proxy",type:"queue"},"proxy","type");(t=>{console.log("~~~ out  ~~~"),n(t)})(t=new Proxy(t,handler))}catch(t){c(t)}})(0,o[0],o[1],o[2],o[3]);break;default:c(`new type [(${e})${t[r]}]`)}}catch(t){c(t)}})(t,o[0],o[1],o[2],o[3]);break;default:c(`new function ${e}`)}});
+import store from '../../../../../../static.frontend/static/html/components/component_modules/staticProperty/staticProperty.mjs'
+let handler = {
+    get: function(obj, prop) {
+        console.log('~~~!!!~~~',obj, prop)
+
+        return obj[prop];
+    },
+    set: function(obj, prop, value) {
+        obj[prop] = value;
+        console.log('~~~!0!~~~',obj[prop], value)
+        switch (prop) {
+            case 'delete':
+
+                break
+            case 'create':
+
+                break
+            case 'update':
+
+                break
+            default:
+                break
+        }
+        return true
+    }
+}
+export default  (obj, func, ...args)=>{
+    return new Promise( function (resolve, reject) {
+        let out = (obj) => {
+            console.log('~~~ out  ~~~')
+            resolve(obj)
+        }
+        let err = (error) => {
+            console.log('~~~ err  ~~~', error)
+            reject(error)
+        }
+        switch (func) {
+            case 'init':
+                (async (obj, props,data) => {
+                    try {
+                        console.log(`app(${func}[(${obj['input']})${obj[props]}]property)`)
+                        switch (obj[props]) {
+                            case 'queue':
+                                (async (obj, props,data) => {
+                                    try {
+                                        let task = await  store({
+                                            input:'proxy',
+                                            type:'queue'
+                                        },'proxy', 'type')
+                                        task = new Proxy(task, handler)
+                                        out(task)
+                                    } catch (e) { err(e) }
+                                })(obj, args[0], args[1], args[2], args[3])
+                                break
+                            default:
+                                err(`new type [(${func})${obj[props]}]`)
+                                break
+                        }
+                    } catch (e) { err(e) }
+                })(obj, args[0], args[1], args[2], args[3])
+                break
+            default:
+                err(`new function ${func}`)
+                break
+        }
+    })
+}
